6bc0f7bf653b6fb6b46b4750da03e5d5e5f5c3c8
Dion Boles <dion.boles@gmail.com>
2022-09-12 14:55:46 -0400

    upper add loweer

--- upper.go
+++ upper.go
@@ -8,5 +8,16 @@ import (
 
 func main() {
 	argsWithProg := os.Args[1:]
-	fmt.Println(strings.ToUpper(strings.Join(argsWithProg, " ")))
+	var check = false
+	for _, a := range argsWithProg {
+		if strings.ToLower(a) == "lower" {
+			check = true
+		}
+	}
+
+	if check == false {
+		fmt.Println(strings.ToUpper(strings.Join(argsWithProg, " ")))
+	} else {
+		fmt.Println(strings.ToLower(strings.Join(argsWithProg, " ")))
+	}
 }
